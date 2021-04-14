0699d71d550eade85bc311f109b0f465c6a6f5e7
MLizzi <michael.lizzi@hotmail.com>
2021-04-14 14:19:34 -0400

    updated with comments on how to group by ['location']'

--- final-project.py
+++ final-project.py
@@ -1,7 +1,12 @@
 from wilds import get_dataset
 from wilds.common.data_loaders import get_train_loader
 import torchvision.transforms as transforms
+from wilds.common.grouper import CombinatorialGrouper
 
-dataset = get_dataset(dataset='iwildcam', download=True)
-train_data = dataset.get_subset('train', transform=transforms.Compose([transforms.Resize((448,448)), transforms.ToTensor()]))
-train_loader = get_train_loader('standard', train_data, batch_size=16)
+if __name__ == '__main__':
+    dataset = get_dataset(dataset='iwildcam', download=True)
+    train_data = dataset.get_subset('train', transform=transforms.Compose([transforms.Resize((448,448)), transforms.ToTensor()]))
+    train_loader = get_train_loader('standard', train_data, batch_size=16)
+
+    grouper = CombinatorialGrouper(dataset, ['location'])
+
