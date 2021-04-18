259d1eb06b2489e89ef754649ffd59faa0fa7868
unknown <michael.lizzi@hotmail.com>
2021-04-18 18:04:08 -0400

    adding first test results, updates to visualize.py

--- visualize.py
+++ visualize.py
@@ -6,23 +6,41 @@ import os
 from pytorch_grad_cam import GradCAM, ScoreCAM, GradCAMPlusPlus, AblationCAM, XGradCAM
 from pytorch_grad_cam.utils.image import show_cam_on_image, preprocess_image
 from torchvision.models import resnet50
+import models
+import torch
 
-model = resnet50(pretrained=True) # TO SWAP OUT WITH OUR MODEL
-target_layer = model.layer4[-1] # TO ADJUST IF OUR MODEL HAS MORE LAYERS (I.E. OUTPUT LAYER NUMBER IS DIFFERENT)
+model = models.load_modified_pre_trained('resnet18', 4)
 
-imagePath = 'D:\Desktop'
-imageName = 'dogs.png' # swap around based on image wanted
+# Place model on device
+# model = model.to(device)
 
+# Load the weights
+model_filename = 'tested_model_20'
+model.load_state_dict(torch.load(model_filename, map_location=torch.device('cpu')))
 
+# model = resnet50(pretrained=True) # TO SWAP OUT WITH OUR MODEL
+target_layer1 = model.layer1[-1] # TO ADJUST IF OUR MODEL HAS MORE LAYERS (I.E. OUTPUT LAYER NUMBER IS DIFFERENT)
+target_layer2 = model.layer2[-1] # TO ADJUST IF OUR MODEL HAS MORE LAYERS (I.E. OUTPUT LAYER NUMBER IS DIFFERENT)
+target_layer3 = model.layer3[-1] # TO ADJUST IF OUR MODEL HAS MORE LAYERS (I.E. OUTPUT LAYER NUMBER IS DIFFERENT)
+target_layer4 = model.layer4[-1] # TO ADJUST IF OUR MODEL HAS MORE LAYERS (I.E. OUTPUT LAYER NUMBER IS DIFFERENT)
+target_layers = [target_layer1, target_layer2, target_layer3, target_layer4]
+imagePath = r'D:\Documents\4th Year\CSC413\Final-Project'
+imageName = 'boar.jpg' # swap around based on image wanted
+
+print(os.path.join(imagePath, imageName))
 rgb_img = cv2.imread(os.path.join(imagePath, imageName), 1)[:, :, ::-1]
 rgb_img = np.float32(rgb_img) / 255
-input_tensor = preprocess_image(rgb_img)#, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+input_tensor = preprocess_image(rgb_img, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 #input_tensor = # ADD IMAGE TENSOR (either through
 
 #Can be GradCAM, ScoreCAM, GradCAMPlusPlus, AblationCAM, XGradCAM
-cam = GradCAMPlusPlus(model=model, target_layer=target_layer, use_cuda=False)
-grayscale_cam = cam(input_tensor=input_tensor, target_category=1)
-visualization = show_cam_on_image(rgb_img, grayscale_cam)
-cv2.imshow('GradCAM++ ' + imageName, visualization)
-cv2.imwrite(os.path.join(imagePath, 'GradCAM++ ' + imageName), visualization) 
-cv2.waitKey(0)
+for i, layer in enumerate(target_layers):
+    cam = GradCAMPlusPlus(model=model, target_layer=layer, use_cuda=False)
+    grayscale_cam = cam(input_tensor=input_tensor, target_category=0)
+    visualization = show_cam_on_image(rgb_img, grayscale_cam)
+    cv2.imshow('GradCAM++ ' + imageName.replace('.jpg',''), visualization)
+    folder = 'GradCAM++_{}_{}'.format(imageName.replace('.jpg',''), model_filename)
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    cv2.imwrite(os.path.join(imagePath, folder, 'GradCAM++_{}_{}_layer{}.jpg'.format(imageName.replace('.jpg',''),model_filename, i+1)), visualization)
+    cv2.waitKey(0)
