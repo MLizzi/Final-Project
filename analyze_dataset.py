339f3760e275a2cf183ba954e86d4bb982d6cee4
btleyre <btleyre@q.vector.local>
2021-04-18 15:36:29 -0400

    Added code for training on overlapping location data.

--- analyze_dataset.py
+++ analyze_dataset.py
@@ -1,6 +1,7 @@
 """ Methods for analyzing the WILDS dataset."""
 import pandas as pd
 import torch
+import numpy as np
 
 from wilds import get_dataset
 
@@ -34,6 +35,16 @@ def create_location_array(train_data, file_path):
             'Puma': loc_data[loc_data[:,-1] == label_list[2]].shape[0],
             'Turkey': loc_data[loc_data[:,-1] == label_list[3]].shape[0]
         }
+        count_dict['Std'] = np.std([count_dict['Boar'], count_dict['Rodent'],
+                                    count_dict['Puma'], count_dict['Turkey']])
+
+        boar_here = 1 if count_dict['Boar'] != 0 else 0
+        rodent_here = 1 if count_dict['Rodent'] != 0 else 0
+        puma_here = 1 if count_dict['Puma'] != 0 else 0
+        turkey_here = 1 if count_dict['Turkey'] != 0 else 0
+
+        overlap = boar_here + rodent_here + puma_here + turkey_here
+        count_dict['Overlap'] = overlap
         dict_list.append(count_dict)
 
     # Convert list of dictionaries into CSV.
@@ -92,8 +103,62 @@ def greedy_location_split(file_path, num_samples):
     return location_lists
 
 
+def find_overlap_locations(file_path, num_samples):
+    """Find a locations that have a high concentration of all 4 labels.
+
+    Parameters:
+    file_path: str. The path to a .csv file created by `create_location_array`.
+    num_samples: int. The min number of samples that each class should have.
+
+    returns: 4-tuple of lists of ints. The lists of locations
+        for each of the classes.
+
+    """
+    # For now, just select 
+    dataframe = pd.read_csv(file_path)
+
+    # Keep track of which locations have been claimed.
+    boar_list = []
+    boar_num = 0
+    rodent_list = []
+    rodent_num = 0
+    puma_list = []
+    puma_num = 0
+    turkey_list = []
+    turkey_num = 0
+
+    sorted_by_std = dataframe.sort_values(by=['Std'])
+
+    for index, row in sorted_by_std.iterrows():
+        if row['Overlap'] > 1:
+
+            if boar_num < num_samples and row['Boar'] != 0:
+                boar_list.append(row['Location'])
+                boar_num += row['Boar']
+            if rodent_num < num_samples and row['Rodent'] != 0:
+                rodent_list.append(row['Location'])
+                rodent_num += row['Rodent']
+            if puma_num < num_samples and row['Puma'] != 0:
+                puma_list.append(row['Location'])
+                puma_num += row['Puma']
+            if turkey_num < num_samples and row['Turkey'] != 0:
+                turkey_list.append(row['Location'])
+                turkey_num += row['Turkey']
+        print("{} {} {} {}".format(boar_num, rodent_num, puma_num, turkey_num))
+
+        if boar_num >= num_samples and rodent_num >= num_samples and \
+                puma_num >= num_samples and turkey_num >= num_samples:
+            break
+
+    if boar_num < num_samples or rodent_num < num_samples or \
+            puma_num < num_samples or turkey_num < num_samples:
+        raise ValueError('Could not satisfy num_samples!')
+
+    return [boar_list, rodent_list, puma_list, turkey_list]
+
+
 if __name__ == "__main__":
     dataset = get_dataset(dataset='iwildcam', download=True)
     train_data = dataset.get_subset('train')
-    #create_location_array(train_data, "./location_counts.csv")
+    # create_location_array(train_data, "./location_counts.csv")
     greedy_location_split("./location_counts.csv", 2000)
---
+++ some_overlap_experiment.py
@@ -0,0 +1,77 @@
+import argparse
+
+import analyze_dataset
+import dataloading
+import models
+
+import torch
+from wilds import get_dataset
+from wilds.common.data_loaders import get_train_loader, get_eval_loader
+import torchvision.transforms as transforms
+
+
+if __name__ == '__main__':
+
+    # Parse the arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_model_type",
+                        help="The type of model.")
+    parser.add_argument("output_model_path",
+                        help="Where to save the model.")
+    # Optional arguments
+    parser.add_argument("-b",
+                        default=80,
+                        help="Batch size.")
+    # Optional arguments
+    parser.add_argument("-e",
+                        default=10,
+                        help="Num epochs.")
+    args = parser.parse_args()
+
+    # Some torch set up
+    torch.manual_seed(1234)
+    # Default device + GPU random seed setting
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+        torch.cuda.manual_seed_all(1234)
+        print(f'Running on GPU: {torch.cuda.get_device_name()}.')
+    else:
+        device = torch.device("cpu")
+
+    dataset = get_dataset(dataset='iwildcam', download=True)
+
+    # Get some info about the label populations in the training data
+    train_data = dataset.get_subset('train')
+    analyze_dataset.create_location_array(train_data, "./data/location_counts.csv")
+    location_lists = analyze_dataset.find_overlap_locations("./data/location_counts.csv", 2000)
+
+    # Create a list of pairs of labels and locations.
+    label_ids = [1, 2, 4, 146]
+    pair_list = []
+    for label, locations in zip(label_ids, location_lists):
+        for loc_id in locations:
+            pair_list.append((label, loc_id))
+
+    # Modify the dataset to only include indices satisfying these criteria.
+    partition_set = dataloading.collect_location_label_pairs(dataset, pair_list, 'train')
+    test_pair_list = [(1, 'all'), (2, 'all'), (4, 'all'), (146, 'all')]
+    partition_test_set = dataloading.collect_location_label_pairs(dataset, test_pair_list, 'test')
+
+    # TODO: Adjust the labels so that they're in the 0 - 3 range?
+    # adjust_ys(partition_set, 5)
+
+    partition_loader = get_train_loader('standard', partition_set, batch_size=int(args.b))
+    eval_loader = get_eval_loader('standard', partition_test_set, batch_size=16)
+        
+    # partition_set = dataloading.collect_location_label_pairs(train_data, [(idx, idx) for idx in range(5)])
+    # partition_loader = get_train_loader('standard', partition_set, batch_size=16)
+
+    model = models.load_modified_pre_trained(args.input_model_type, 4)
+
+    # Place model on device
+    model = model.to(device)
+
+    models.fine_tune_model(model, partition_loader, eval_loader, args.output_model_path, device, num_epochs=int(args.e))
+
+    print(partition_set.metadata_array.shape)
+    print(train_data.metadata_array.shape)
